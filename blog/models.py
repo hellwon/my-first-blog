35b988e7fddd224190a1993666987393fe24c6bb
won.lee <won.lee@rgpkorea.co.kr>
2017-11-01 18:03:51 +0900

    - add comment

--- blog/models.py
+++ blog/models.py
@@ -21,6 +21,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
@@ -35,3 +38,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
